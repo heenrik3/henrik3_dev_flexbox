c1a4c5840d723679cca9355a13d29fc9ba18a784
henrik3 <henriqlh@gmail.com>
2021-03-25 22:38:00 -0300

    18

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 let title = 'Henrik3';
 
 document.title = title.toUpperCase();
+
+document.getElementById("popup-call").addEventListener("click", showPopup);
+document.getElementById("popup").addEventListener("click", hidePopup);
+function showPopup() {
+  document.getElementById("popup").style.zIndex= 1;
+  document.getElementById("popup").style.opacity = 1;
+  document.getElementById("popup").style.visibility = 'visible';
+}
+
+function hidePopup() {
+  document.getElementById("popup").style.zIndex= -1;
+  document.getElementById("popup").style.opacity = 0;
+  document.getElementById("popup").style.visibility = 'invisible';
+
+}
